bac17259e2099e6919cb3476ff1fcd268880e577
Stas SUȘCOV <stas@net.utcluj.ro>
2010-11-05 19:41:41 +0200

    Ready for beta

--- lfc/1/checker.py
+++ lfc/1/checker.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-
+"""
+A syntax checker for a python alike programming language
+See main.lang file for source code example
+The checker must generate the codes table and table of symbols for current source file
+"""
 import sys
 import re
+from pprint import pprint
 
 def checker(text):
     """
@@ -12,23 +17,17 @@ def checker(text):
     On failure it will output the position in the <text> where pattern fails matching
     """
     pattern = re.compile(r"""
-        (?P<read_operator>(READ)|(read))
-        |(?P<print_operator>(PRINT)|(print))
-        |(?P<if_operator>(IF)|(if))
-        |(?P<else_operator>(ELSE)|(else))
-        |(?P<endif_operator>(ENDIF)|(endif))
-        |(?P<while_operator>(WHILE)|(while))
-        |(?P<endwhile_operator>(ENDWHILE)|(endwhile))
-        |(?P<identifier>[A-Z_][A-Z0-9_]*)
-        |(?P<variable>[a-zA-Z][a-zA-Z0-9_]+)
+        (?P<reserved_key>[A-Z_][A-Z0-9_]*)
+        |(?P<identifier>[a-zA-Z][a-zA-Z0-9_]+)
         |(?P<digits>[0-9]+)
         |(?P<string>\'[a-zA-Z0-9_(.)]+\')
         |(?P<array>\[(\'(\d+)|(\w+)|(.)+\')\])
         |(?P<newline>\n)
         |(?P<whitespace>\s+)
+        |(?P<special_char>([=][=])|([<][=])|([>][=])|[<]|[>]|[+]|[-]|[*]|[/]|[=])
         |(?P<match>[=][=])
         |(?P<less_then>[<][=])
-        |(?P<more_then>[<][=])
+        |(?P<more_then>[>][=])
         |(?P<less>[<])
         |(?P<more>[>])
         |(?P<plus>[+])
@@ -51,6 +50,26 @@ def checker(text):
     if pos != len(text):
         print 'Syntax error at ', pos, ' of ', len(text)
 
+def gen_table(data):
+    """
+    Check if <data> suid our simbol table and return an array with its code and simbol value
+    """
+    type = data[0]
+    value = data[1]
+    
+    ts = dict(
+        reserved_key = -1,
+        identifier = 0,
+        string = 1,
+        digits = 2,
+        array = 3,
+        )
+    
+    if type is not 'whitespace' or 'special_char' or 'newline':
+        if ts.has_key(type):
+            return ts.get(type), value
+
+
 # Start checking...
 def main():
     """
@@ -59,9 +78,16 @@ def main():
     This will parse the <file> and call checker() for each line
     Before each call, the <line> number will be outputed.
     """
-    file = "main.lang"
+    tc = list()
+    st = list()
+    
+    file = None
+    
     if len(sys.argv) > 1:
         file = sys.argv[1]
+    else:
+        print 'Usage: ', sys.argv[0], '<source file to check>'
+        sys.exit(0)
     
     print 'Checking ', file, '...'
     
@@ -71,9 +97,30 @@ def main():
     for l in lines:
         print 'Checking line: ', curr_line
         for r in checker(l):
-            print r
+            atom = gen_table(r)
+            if atom:
+                st.append(r[1])
+                tc.append(atom)
         curr_line += 1
-        print
+    st = list(set(st))
+    
+    print
+    print 'Printing TC'
+    print '###########'
+    for item in st:
+        # position in tc and position in st
+        for code in tc:
+            if code[1] is item:
+                print code[0], st.index(item)
+    
+    print
+    print 'Printing ST'
+    print '###########'
+    for simbol in st:
+        print st.index(simbol), simbol
+    
+    # For debugging, print the not joined TC
+    #pprint(tc)
 
 if __name__ == "__main__":
     main()
