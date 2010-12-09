5801c98b5ea89c4c262fbdf5832618981bfbc50c
Stas SUȘCOV <stas@net.utcluj.ro>
2010-12-09 15:47:06 +0200

    Sorting works for linux

--- osd/4.1/main.c
+++ osd/4.1/main.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <assert.h>
+#include <time.h>
 
 #if defined (__SVR4) && defined (__sun)
 #include <thread.h>
@@ -13,25 +14,50 @@
 #define MAX_NUMS 100
 
 int array[MAX_NUMS];
-int f_half[MAX_NUMS/2];
-int s_half[MAX_NUMS/2];
+int *f_half;
+int *s_half;
 int elements;
 
-void print_array(int array[])
+void print_array(int array[], int len)
 {
     int i;
     printf("ARRAY[");
-    for(i=0;i<=elements;i++)
+    for(i=0;i<len;i++)
         printf("%d,", array[i]);
     printf("]\n");
+}
+
+int
+intcmp(const void *p1, const void *p2)
+{
+    int i = *((int *)p1);
+    int j = *((int *)p2);
+
+    if (i > j)
+        return (1);
+    if (i < j)
+        return (-1);
+    return (0);
 }
 
 void *
 sorting(void *thread_id)
 {
-    int tid;
+    int tid, i;
     tid = *((int *) thread_id);
 
+    if(tid == 1)
+    {
+        f_half = malloc(elements/2);
+        f_half = &array[0];
+        qsort(f_half, elements/2, sizeof(int), intcmp);
+    }
+    else
+    {
+        s_half = malloc(elements/2);
+        s_half = &array[elements/2];
+        qsort(s_half, elements/2, sizeof(int), intcmp);
+    }
     return NULL;
 }
 
@@ -57,6 +83,12 @@ do_sort(void *thread_id)
         rc = pthread_join(threads[i], NULL);
         assert(0 == rc);
     }
+    
+    // Concatenate
+    array[0] = f_half[0];
+    array[elements/2] = s_half[0];
+    qsort(array, elements, sizeof(int), intcmp);
+    print_array(array, elements);
 
     printf("do_sort: Thread %d terminates.\n", tid);
     return NULL;
@@ -79,6 +111,9 @@ read_file(char *filename)
 int
 main (int argc, char *argv[])
 {
+    struct timespec start, finish;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     int rc, i;
     char *filename = 0;
     pthread_t thread;
@@ -100,13 +135,15 @@ main (int argc, char *argv[])
 
     printf("Sorting from %s...\n", filename);
     read_file(filename);
-    print_array(array);
-    
+    print_array(array, elements);
+
     rc = pthread_create(&thread, NULL, do_sort, (void *) &thread_arg);
     assert(0 == rc);
 
     rc = pthread_join(thread, NULL);
     assert(0 == rc);
 
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    printf("Used time: %ld", (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1000000000.0 );
     exit(EXIT_SUCCESS);
 }
